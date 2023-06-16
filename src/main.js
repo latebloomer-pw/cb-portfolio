42e290e54dd2f3efc5e979ddd1d7d8d933ea810a
pete <pete@latebloomer.studio>
2023-06-16 11:27:30 -0400

    im going thru
    changes

--- src/main.js
+++ src/main.js
@@ -1,20 +1,19 @@
 import './styles/style.css'
 import Lenis from '@studio-freight/lenis'
 import { gsap } from 'gsap'
+//import { Timeline } from 'gsap/gsap-core'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
-//import $ from 'jquery'
-const lenis = new Lenis()
-// //const getTrigger = (index) => $('.container').eq(index)
-// const getTarget = (index) => {
-//   let targetElement = $('.scroll-item')
-//   if (index === 0) {
-//     return targetElement.slice(0, 3)
-//   } else if (index === 1) {
-//     return targetElement.slice(3, 6)
-//   } else {
-//     return targetElement.slice(6, 9)
-//   }
-// }
+import $ from 'jquery'
+
+// WINDOW RESIZE LISTENER
+// let isDesktop = window.matchMedia('(min-width: 768px)').matches
+// window.addEventListener('resize', () => {
+//   isDesktop = window.matchMedia('(min-width: 768px)').matches
+// })
+// // ===============
+
+const lenis = new Lenis({ lerp: 0.075 })
+
 function raf(time) {
   lenis.raf(time)
   requestAnimationFrame(raf)
@@ -23,20 +22,38 @@ function raf(time) {
 requestAnimationFrame(raf)
 
 gsap.registerPlugin(ScrollTrigger)
+// MENU SCROLL EFFECT
+$('.featured-item').each(function () {
+  // each project wrapper (i.e. CMS item) triggers link hover state
 
-const projectTriggers = document.querySelectorAll('.container')
+  // inner function to DRY things up
+  const enterActions = (element) => {
+    $('.menu-current').text(element.attr('menu-name'))
+  }
 
-projectTriggers.forEach(addTimeline)
+  let triggerElement = $(this)
+  gsap.timeline({
+    scrollTrigger: {
+      trigger: triggerElement,
+      // trigger element - viewport
+      start: 'top top+=100',
+      end: 'bottom bottom-=200', // shrink available trigger space for faster response time
+      onEnter: () => enterActions(triggerElement),
+      onEnterBack: () => enterActions(triggerElement),
+    },
+  })
+})
 
-function addTimeline(project) {
-  const images = project.querySelectorAll('.scroll-anim')
-  gsap
-    .timeline({
-      scrollTrigger: {
-        trigger: project,
-        start: 'top +1950',
-        scrub: true,
-      },
-    })
-    .fromTo(images, { scale: 0.5 }, { scale: 1.0075 })
-}
+// BACK CLICK BUTTON
+$('.button.back').on('click', () => {
+  history.back()
+})
+
+// MENU CLICK BUTTON
+$('.menu-center').on('click', function () {
+  !$('.nav').hasClass('open')
+    ? $('.menu-text').text('Close Menu')
+    : $('.menu-text').text('Menu')
+  $('.nav').toggleClass('open')
+  $('.hidden-menu').toggleClass('open')
+})
